cd20446211fe65b2553c8f3d08f203bdfd76670e
Jeffersonsh20 <79332543+Jeffersonsh20@users.noreply.github.com>
2021-08-07 14:11:41 -0600

    Add files via upload

---
+++ PorrateoFactura.java
@@ -0,0 +1,20 @@
+
+package porrateofactura;
+
+import datos.Principal;
+
+
+public class PorrateoFactura {
+
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        
+        principal.iniciar();
+    }
+    
+}
