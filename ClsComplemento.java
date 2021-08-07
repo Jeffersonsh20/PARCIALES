02ae46a41486ae80214b311e1fe4232900b1c6de
Jeffersonsh20 <79332543+Jeffersonsh20@users.noreply.github.com>
2021-08-06 22:01:56 -0600

    Add files via upload

---
+++ ClsComplemento.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra.pkg2.parcial.pkg1;
+
+/**
+ *
+ * @author Alexis
+ */
+public class ClsComplemento {
+
+    void operaciones(ClsDatos datos) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void imprimirDecorado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void AgregaVendedores(ClsDatos datos) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ ClsDatos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra.pkg2.parcial.pkg1;
+
+/**
+ *
+ * @author Alexis
+ */
+public class ClsDatos {
+
+    static String Kilogramos;
+    String NombreProducto;
+    String NumeroUnidades;
+    String PrecioUnitario;
+    
+}
